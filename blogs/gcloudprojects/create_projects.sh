f15e177fe74eb6e3ff3ec1fb3c7b1c1441ae5636
brokenairplane <phil@brokenairplane.com>
2017-01-17 06:21:23 -0800

    Adding logging messages, removing sudo command and logging to file

--- blogs/gcloudprojects/create_projects.sh
+++ blogs/gcloudprojects/create_projects.sh
@@ -12,7 +12,7 @@ PROJECT_PREFIX=$1
 shift
 EMAILS=$@
 
-sudo gcloud components update
+gcloud components update
 gcloud components install alpha
 
 for EMAIL in $EMAILS; do
@@ -30,19 +30,23 @@ for EMAIL in $EMAILS; do
    gcloud alpha projects set-iam-policy $PROJECT_ID iam.json.new
 
    # billing
+   echo "Enabling Billing"
    gcloud alpha billing accounts projects link $PROJECT_ID --account-id=$ACCOUNT_ID
    
    # enable APIs
+   echo "Enabling Compute and ML APIs"
    gcloud beta service-management enable compute_component --project=$PROJECT_ID
    gcloud beta service-management enable ml.googleapis.com --project=$PROJECT_ID
    
    # add service accounts for ML
+   echo "Adding ML service account"
    gcloud beta ml init-project --project=$PROJECT_ID
    
    # add firewall rule to allow Datalab
+   echo "Adding new firewall rule"
    gcloud compute firewall-rules create allow-datalab --allow=tcp:22,tcp:8081
    
    # output the email, project id, and a link to the project console
-   printf "%s %s https://console.cloud.google.com/home/dashboard?project=%s\n" $EMAIL $PROJECT_ID $PROJECT_ID
+   printf "%s %s https://console.cloud.google.com/home/dashboard?project=%s\n" $EMAIL $PROJECT_ID $PROJECT_ID | tee account-list.txt
 
 done
